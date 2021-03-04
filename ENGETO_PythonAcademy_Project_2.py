bf0e26fe343099afd9136350af18eab15a761b5d
David <davidwlangr@gmail.com>
2021-03-04 22:00:34 +0100

    Simplification of 4 digits generator script Added game time Added game guesses count

--- ENGETO_PythonAcademy_Project_2.py
+++ ENGETO_PythonAcademy_Project_2.py
@@ -1,7 +1,10 @@
 
-from random import choice
+from random import sample
+from time import time
 
 def main():
+    start = time()
+
     text0 = "I've generated a random 4 digits number for you."
     text1 = "Let's play a bulls and cows game."
 
@@ -11,56 +14,58 @@ def main():
     print(text1)
     oddelovac(text0)
 
+    guess_count = 0
+
     #Vygenerovani 4 cislic
     lst_4 = random_4()
 
     #Vyhodnoceni tipu uzivatele
     tipovani = True
     while tipovani:
-
+        guess_count += 1
         # Vstup a kontrola vstupu uzivatele
         player_input_string = vstup_kontrola()
 
         #Prevod vstupu uzivatele na list cisel
         player_input_lst = vstup_prevod(player_input_string)
 
+        #Kontrola, zda již neuhodl správně 4-číslí
         if player_input_lst == lst_4:
             print(f"Correct, you've guessed the right number.")
-            exit()
+            break
         else:
+            #Kontrola shody čísel a umístění
             bull_cow(player_input_lst, lst_4)
 
+    end = time()
 
+    #Čas hry převedený do formátu mm : ss
+    game_time = game_time_format(end - start)
+    print(f"You had {guess_count} {['guess', 'guesses'][guess_count != 1]}.")
+    print(f"Your game took: {game_time}")
 
-def oddelovac(text):
+def oddelovac(text: str) -> str:
     print(len(text) * "-")
 
 
 #def fce generovani 4 cisel (zadne se neopakuje, prvni nesmi byt 0)
-def random_4():
-    lst_random_4 = []
-    no_lst = [0,1,2,3,4,5,6,7,8,9]
-
-    #Vyber prniho cisla, aby nebylo "0". Odstraneni cisla, aby se neopakovalo.
-    first = choice(no_lst[1:])
-    lst_random_4.append(first)
-    no_lst.remove(first)
-
-    #Vyber zbylych cisel. Po kazdem vyberu odstraneni, aby se neopakovala.
-    for x in range(3):
-        number = choice(no_lst)
-        lst_random_4.append(number)
-        no_lst.remove(number)
-
-    return lst_random_4
+def random_4() -> list:
+    check = True
+    #generuje 4-cisly (kazde cislo jine), dokud neplati podminka, ze 1. cislo se nerovna 0
+    while check:
+        lst = [cislo for cislo in sample(range(10), 4)]
+        if lst[0] != 0:
+            break
+    return lst
 
 
 #def fce vstup a kontrola hracem zadaneho cisla, ze je presne 4 znaky, znaky jsou cisla a prvni neni 0
-def vstup_kontrola():
+def vstup_kontrola() -> str:
     kontrola = True
     while kontrola:
         player_input = input("Enter a number: ")
         if len(player_input) != 4 or player_input[0] == '0' or not player_input.isdigit() or len(set(player_input)) != 4:
+            print("Wrong input! Try again.")
             continue
         else:
             kontrola = False
@@ -68,14 +73,14 @@ def vstup_kontrola():
     return player_input
 
 #def fce prevedeni uzivatelova vstupu (stringu) na list cisel
-def vstup_prevod(string):
+def vstup_prevod(string: str) -> list:
     lst_vstup = []
     for char in string:
         lst_vstup.append(int(char))
     return lst_vstup
 
 #def fce kontrola shody cisel a umisteni, kontrola poctu spravnych cisel
-def bull_cow(player, game_no):
+def bull_cow(player: list, game_no: list) -> str:
     player_no = list(player)
     spravna_pozice = 0
     spravne_cislo = 0
@@ -94,11 +99,13 @@ def bull_cow(player, game_no):
     vystup = [f"{spravna_pozice} {byk[spravna_pozice == 1]}, {spravne_cislo} {krava[spravne_cislo == 1]}"]
     print(vystup)
 
+
+def game_time_format(time) -> str:
+    output = f"{round(time // 60)} min : {(time % 60):.0f} sec"
+    return output
+
+
 main()
-#def casu hry
 
-#def poctu odhadu
 
-#opravit popisky parametru
 
-#Dopsat info o proměnných k definovaným funkcím
